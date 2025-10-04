4bb51044eb0eb7a323d24535ab4ee98e298e907b
Shahriar Ahmed <chatpgt7@gmail.com>
2025-10-05 00:29:33 +0600

    added auth but some bugs to be fixedz

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@cl
 
 const Header = () => {
   return (
-    <div><SignedOut>
+    <div className='fixed top-0'><SignedOut>
               <SignInButton />
               <SignUpButton>
                 <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
--- middleware.js
+++ middleware.js
@@ -11,15 +11,13 @@ export default clerkMiddleware(async (auth, req) => {
 
   if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
-
     redirectToSignIn();
   }
 });
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
